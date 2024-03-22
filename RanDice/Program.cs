4e94238fb4e16644754fa81c28b257e699e69ef8
matv <matv.oficial@gmail.com>
2024-03-22 23:35:48 +0000

    RanDice finished

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -4,9 +4,26 @@ namespace RanDice
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+
+            int sum = SumDice(n, s);
+            Console.WriteLine(sum);
+        }
+
+        static int SumDice(int rolls, int seed)
+        {
+            int result = 0;
+            Random r = new Random(seed);
+            for (int i = 0; i < rolls; i++)
+            {
+                result += r.Next(1, 7);
+            }
+            return result;
         }
     }
 }
