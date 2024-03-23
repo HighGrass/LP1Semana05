958c51fa493212d0b4a4cc1c9ee9f472fdcbdf36
matv <matv.oficial@gmail.com>
2024-03-23 00:04:06 +0000

    MyGame - (Main) -  choose and display enemies name

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -4,10 +4,26 @@ namespace MyGame
 {
     class Program
     {
-         static void Main(string[] args)
+        static int enemyAmount;
+        static Enemy[] enemies;
+
+        static void Main(string[] args)
         {
-            
-            
+
+            enemyAmount = int.Parse(args[0]);
+            enemies = new Enemy[enemyAmount];
+
+            for (int i = 0; i < enemyAmount; i++)
+            {
+                string enemyName;
+                Console.Write("Choose the name of the enemy " + (i + 1) + ":");
+                enemyName = Console.ReadLine();
+                enemies[i] = new Enemy(enemyName);
+            }
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                Console.WriteLine("{0} {1} {2}", enemies[i].GetName(), enemies[i].GetHealth(), enemies[i].GetShield());
+            }
         }
     }
 }
